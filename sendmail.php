4225ecebd5d7de577f8565c294890010c42326aa
Alaicev <altarev123456@mail.ru>
2025-05-07 20:16:04 +0400

    DDD

--- sendmail.php
+++ sendmail.php
@@ -1,18 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $fam = $_POST['fam'];
-    $otc = $_POST['otc'];
-    
-    $to = "altarev12345@gmail.com"; 
-    $subject = "Новое сообщение с формы";
-    $message = "Имя: $name\nФамилия: $fam\nОтчество: $otc";
-    $headers = "From: morshnev199@mail.ru";
-    
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Сообщение успешно отправлено!";
-    } else {
-        echo "Ошибка при отправке сообщения.";
-    }
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = 'твой@email.com';
+    $subject = 'Новое письмо от ' . $name;
+    $headers = "From: $email";
+
+    mail($to, $subject, $message, $headers);
+    echo "Письмо ебнуло на сервер!";
+} else {
+    echo "Чё-то пошло не так, братан.";
 }
 ?>
